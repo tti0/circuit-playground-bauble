050e7d255bb2bec1c69ba78dc361cb57d4971dde
tti0 <14841064+tti0@users.noreply.github.com>
2021-10-29 14:44:30 +0100

    Add 'Nutcracker' pattern, rainbow, and ww twinkle

--- circuit-playground-bauble.ino
+++ circuit-playground-bauble.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_CircuitPlayground.h>
 #include "twinkleIntensityFrame.h"
 #include "colourBrightnessModifier.h"
+#include "nutcrackerPattern.h"
 
 void setup() {
   CircuitPlayground.begin();
@@ -16,7 +17,7 @@ void loop() {
   // Generate a random offset starting frame for each LED
   int offsets[10] = {random(300), random(300), random(300), random(300), random(300), random(300), random(300), random(300), random(300), random(300)};
   // For each frame of animation (1)
-  for (int i = 0; i <= 5000; i++) {
+  for (int i = 0; i <= 3000; i++) {
     // For each LED
     for (int j = 0; j <= 9; j++) {
       int myCurrentFrame = (i + offsets[j]) % twinkleHighestNumberedFrame;
@@ -24,16 +25,39 @@ void loop() {
       CircuitPlayground.setPixelColor(j, modifiedColourComponent(180, myCurrentIntensity), modifiedColourComponent(180, myCurrentIntensity), modifiedColourComponent(255, myCurrentIntensity));
     }  
   }
-}
 
-// (1) cool white twinkle
-// (2) nutcracker flash (r/g/w)
-// (3) blue gradient
-// (2)
-// (4) warm white twinkle
-// (2)
-// (5) rainbow
-// (2)
+  // (2) Nutcracker flash (r/g/w)
+  doNutcrackerFlash();
+
+  // (3) Warm white twinkle
+  // For each frame of animation (1)
+  for (int i = 0; i <= 3000; i++) {
+    // For each LED
+    for (int j = 0; j <= 9; j++) {
+      int myCurrentFrame = (i + offsets[j]) % twinkleHighestNumberedFrame;
+      byte myCurrentIntensity = intensityAtFrame(myCurrentFrame, twinkleMinMaxHoldFrames);
+      CircuitPlayground.setPixelColor(j, modifiedColourComponent(255, myCurrentIntensity), modifiedColourComponent(220, myCurrentIntensity), modifiedColourComponent(50, myCurrentIntensity));
+    }
+  }
+
+  // (2) Nutcracker flash (r/g/w)
+  doNutcrackerFlash();
+
+  // (4) Rainbow cycle
+  // Based on https://github.com/adafruit/Adafruit_CircuitPlayground/blob/master/examples/mega_demo/RainbowCycleDemo.h
+  for(int i = 0; i <= 3000; i++) {
+    // Make an offset based on the current millisecond count scaled by the current speed.
+    uint32_t offset = millis() / 10;
+    // Loop through each pixel and set it to an incremental color wheel value.
+    for(int j = 0; j < 10; j++) {
+      CircuitPlayground.setPixelColor(j, CircuitPlayground.colorWheel(((j * 256 / 10) + offset) & 255));
+    }
+  }
+
+  // (2) Nutcracker flash (r/g/w)
+  doNutcrackerFlash();
+  
+}
 
 // if switch on
 // either button interrupt
